101ba79bea47dec12d09d1eced58e1e2eb59b272
Scott <ScottAdelman97@gmail.com>
2022-07-05 09:31:17 -0600

    simple story

---
+++ Method_Demo/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("What is your name?");
+var name = Console.ReadLine();
+
+Console.WriteLine("Do you have a dog");
+var dog = Console.ReadLine();
+
+Console.WriteLine("What is your dog's name?");
+var dogName = Console.ReadLine(); 
+
+Console.WriteLine("What is Loki's Favorite food?");
+var lokiFavoriteFood = Console.ReadLine();
+
+Console.WriteLine("WARNING: INTRUDER DETECTED. PLEASE ENTER PASSCODE");
+var warning = Console.ReadLine();
+
+Console.WriteLine("Password Denied, System Shutdown Initiated. Enter emergency code to abort shutdown");
+Console.ReadLine();
+
+Console.WriteLine("3... 2... 1...");
+Console.WriteLine("SYSTEM TERMIATED");
+
+
